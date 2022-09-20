55e9a1ec06d98c52ca4add41a304b0df6889de92
laminemessaci <lamine.messaci18@gmail.com>
2022-09-20 11:15:48 +0200

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/index.jsx";
 import Navigation from "./navigation/index.js";
 
 function App() {
   return (
     <>
       <div className="content-wrapper">
+        <Header />
         <Navigation />
       </div>
     </>
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Banner from "../../components/Banner";
-import Header from "../../components/Header/index.jsx";
+
 import "./style.css";
 const Home = () => {
   return (
     <div className="Home">
-      <Header  />
       <Banner
         imgSource="../assets/coast-landscape.jpeg"
         altText="Un paysage sauvage de bord de mer"
