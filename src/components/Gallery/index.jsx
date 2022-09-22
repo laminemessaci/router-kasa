56db0c614dadc7a1ecbeb23772fa1237aecc7f80
laminemessaci <lamine.messaci18@gmail.com>
2022-09-22 11:35:42 +0200

    Add Gallery component

---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
+import Thumbnail from "../thumbnail/index.js";
+import { PropertyAdvertisement } from "../../model/index.js";
+
+function Gallery({ advertisements, ...props }) {
+  console.log(advertisements);
+  return (
+    <div className="gallery">
+      <ul>
+        {advertisements.map(({ id, cover, title, location }) => (
+          <Thumbnail
+            key={`thumbnail-${id}`}
+            id={id}
+            cover={cover}
+            title={title}
+            location={location}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Gallery.propTypes = {
+  advertisements: PropTypes.arrayOf(PropTypes.instanceOf(PropertyAdvertisement))
+    .isRequired,
+};
+
+export default Gallery;
