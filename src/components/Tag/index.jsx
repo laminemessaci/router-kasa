ea9f4ee2d33c5e89f2c19a7415657525c2df21cf
laminemessaci <lamine.messaci18@gmail.com>
2022-09-30 19:39:47 +0200

    Add PropType for Tag comp

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PropTypes } from "prop-types";
 
 import "./style.css";
 
@@ -10,4 +11,7 @@ function Tag({ tag }) {
   );
 }
 
+Tag.propTypes = {
+  tag: PropTypes.string.isRequired,
+};
 export default Tag;
